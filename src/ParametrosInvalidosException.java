b7d5187ea218ca15f0c62cafcec45afdc915d0b6
GustavoRodrigues-Dev <luizgust_rs@yahoo.com.br>
2024-06-13 18:08:08 -0300

    Alteração na mensagem pra padronizar

--- src/ParametrosInvalidosException.java
+++ src/ParametrosInvalidosException.java
@@ -1,5 +1,5 @@
 public class ParametrosInvalidosException extends Exception {
-    public ParametrosInvalidosException(String message) {
-        super(message);
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
     }
 }
